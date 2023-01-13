084029982a637ea745ad88f5ef071610f24fe5ad
orttak <orttak@gmail.com>
2023-01-13 18:08:03 +0100

    update

--- IP-tracking-vue3/app/vite.config.js
+++ IP-tracking-vue3/app/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ip-tracking-app/',
+  base: '/frontend-training/',
   plugins: [vue()],
   resolve: {
     alias: {
